705f4013fb73d58066e283d840ee04a3ec78a650
User <user@Users-MacBook-Air.local>
2024-02-21 14:24:40 +0800

    home image

--- food-client/src/components/HomeBackground/index.tsx
+++ food-client/src/components/HomeBackground/index.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import { Grid, Container, Typography } from "@mui/material";
+import { Grid, Container, Typography, Box } from "@mui/material";
 import FooterIcon from "../../../public/images/FooterIcon";
+import { relative } from "path";
 
 const HomeBackground = () => {
   return (
     <Grid
+      container
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
       sx={{
         background: "#18ba51",
+        width: "100%",
+        height: 500,
+        backgroundImage: `url(${"/images/bg-footer.png"})`,
       }}
-    ></Grid>
+    >
+      <Grid container display={"flex"} justifyContent={"space-evenly"}>
+        <Grid item></Grid>
+        <Grid item>asda</Grid>
+      </Grid>
+    </Grid>
   );
 };
 
