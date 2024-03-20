0daf7b1c5c6e3982a5b149a544297ad17abd38bd
Галсанпунцаг Бат-Орших <bgalsaa20@gmail.com>
2024-03-20 19:32:40 +0800

    bug edited

--- food-server/src/utils/multer.ts
+++ food-server/src/utils/multer.ts
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log("d", path.join(__dirname, "../uploads"));
 
-    cb(null, path.join(__dirname, "../uploads/"));
+    cb(null, path.join(__dirname, "../upload/"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
