5395e72814689838f47ce77efa5e416a8dc117bd
Галсанпунцаг Бат-Орших <bgalsaa20@gmail.com>
2024-03-20 19:38:34 +0800

    aa

--- food-server/src/utils/multer.ts
+++ food-server/src/utils/multer.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    console.log("d", path.join(__dirname, "../uploads"));
+    console.log("d", path.join(__dirname, "../upload"));
 
     cb(null, path.join(__dirname, "../upload/"));
   },
